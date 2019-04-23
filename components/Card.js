20ee2b621a7eae4143e6101faab55316f13dc157
Lucas <lucasdomi@globo.com>
2019-04-23 02:10:17 -0300

    feat/ Adicionando deck, perguntas, respostas e comportamento de mais de um card

--- components/Card.js
+++ components/Card.js
@@ -1,47 +1,64 @@
 import React from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native'
-import {widthDevice, heigthDevice} from '../helpers/Dimension';
-import Response from './Response';
+import {heigthDevice} from '../helpers/Dimension';
+import CardFlip from 'react-native-card-flip';
 
 const CardView = styled.View`
   box-shadow: 5px 5px 5px rgba(0,0,0,1);
 `
 
 export default class Card extends React.Component {
-    render() {
-      return (
-        <CardView style={styles.cardView}>
-          <Text style={{ textAlign: 'center', fontSize: 25 }}>Life?</Text>
-          <Response/>
-        </CardView>
-      );
-    }
+  handleVote = ( card, goToNext ) => {
+    card.flip()
+    goToNext( heigthDevice )
+  }
+  render() {
+    const { item, goToNext } = this.props
+    return (
+      <CardFlip ref={ ( card ) => this['card' + item.id] = card } style={{ height: heigthDevice }} onFlipEnd={ this.fliped }>
+      <CardView style={ styles.cardView }>
+        <Text style={styles.cardQuestion}>{ item.question }</Text>
+        <TouchableOpacity onPress={ () => this[`card${item.id}`].flip() } >
+          <Text style={styles.cardLink}>Check the answer!</Text>
+        </TouchableOpacity>
+      </CardView>
+      <CardView style={styles.cardView}>
+        <Text style={styles.cardQuestion}>{ item.answer }</Text>
+        <Button title='Yes' color='green' onPress={ () => this.handleVote( this[`card${item.id}`], goToNext )} />
+        <View style={{ marginBottom: 10 }} />
+        <Button title='No' color='red' onPress={ () => this.handleVote( this[`card${item.id}`], goToNext ) }/>
+      </CardView>
+    </CardFlip>
+    );
+
+  }
   }
 
   const styles = StyleSheet.create({
     cardView: {
       flex: 1,
-      color: 'red',
-      backgroundColor: '#eeeeee',
       padding: 20,
-      marginTop: heigthDevice * 0.1,
-      marginRight: widthDevice* 0.1,
-      marginLeft: widthDevice* 0.1,
-      marginBottom: heigthDevice* 0.1,
-      borderRadius: 7,
-      textAlign: 'center',
       justifyContent: 'center',
+      backgroundColor: '#B0E0E6',
+      height: heigthDevice*0.8,
+      marginTop: heigthDevice*0.1,
+      marginBottom: heigthDevice*0.1,
     },
     cardQuestion: {
       color: '#111',
-      fontSize: 18,
-      marginBottom: '15',
+      fontSize: 25,
+      marginBottom: 15,
       textAlign: 'center',
     },
     responseCard: {
       textAlign: 'center',
       color: 'blue',
       marginTop: 5,
+    },
+    cardLink: {
+      textAlign: 'center',
+      color: '#0E79B2',
+      marginTop: 10,
     }
   });
--- views/Deck.js
+++ views/Deck.js
@@ -1,13 +1,77 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, ScrollView, Dimensions, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import Card from '../components/Card'
+import {widthDevice, heigthDevice} from '../helpers/Dimension';
+
+const deckQuestions = { questions: [
+  {
+    id: 0,
+    question: 'What is React?',
+    answer: 'A library for managing user interfaces'
+  },
+  {
+    id: 1,
+    question: 'Where do you make Ajax requests in React?',
+    answer: 'The componentDidMount lifecycle event'
+  },
+  {
+    id: 2,
+    question: 'What is a closure?',
+    answer: 'The combination of a function and the lexical environment within which that function was declared.'
+  }
+]}
 
 export default class Deck extends React.Component {
+  state = {
+    scrollHeight: 0,
+  }
+  scrollTo = scrollSize => {
+    this.setState( state => ({
+      ...state,
+      scrollHeight: state.scrollHeight + scrollSize,
+    }), () =>{
+      this.scroll.scrollTo({x: 0, y: this.state.scrollHeight, animated: true});
+    })
+  }
+  restartQuiz = () => {
+    this.setState( state => ({
+      scrollHeight: 0,
+    }), () => {
+      this.scrollTo(0)
+    })
+  }
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Card />
-      </View>
+      <ScrollView ref={(c) => this.scroll = c} scrollEnabled={false} style={{ flex: 1 }}>
+        <FlatList
+          data={ deckQuestions.questions }
+          renderItem={ ({item}) => <Card item={item} goToNext={ this.scrollTo }/> }
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <View style={styles.cardView}>
+          <Text style={styles.textCongratulations}> Congratulations!!</Text>
+          <TouchableOpacity onPress={ this.restartQuiz } >
+            <Text>Restart quiz!</Text>
+          </TouchableOpacity>
+        </View>
+    </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cardView: {
+    flex: 1,
+    height: heigthDevice*0.8, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: '#74B3CE',
+    marginTop: heigthDevice*0.1,
+    marginBottom: heigthDevice*0.1,
+    width: widthDevice * 0.8,
+  },
+  textCongratulations: {
+    fontSize: 25,
+    color: '#fff',
+  }
+});
